ad6e7046158035032ed8bd8e912d81ce9b227506
yaroofie <yaroofie@gmail.com>
2022-12-17 15:43:30 +0330

    feat : Add geography page header

--- src/components/5-Pages/Geography.jsx
+++ src/components/5-Pages/Geography.jsx
@@ -1,13 +1,14 @@
+import { Box } from '@mui/material';
 import React, { useEffect } from 'react';
+import Header from "../3-Organisms/Header";
 
 export default function Geography ()
 {
-   useEffect( () =>
-   {
-      document.title = "Geography Page";
-   }, [] );
+   useEffect( () => { document.title = "Geography Page"; }, [] );
 
    return (
-      <div>Geography</div>
+      <Box m="20px">
+         <Header title="Geography" subtitle="Simple Geography Chart" />
+      </Box>
    );
 }
