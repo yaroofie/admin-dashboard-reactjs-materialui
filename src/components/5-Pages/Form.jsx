190b24199029560dc5d923651c249db60ff4f6e9
yaroofie <yaroofie@gmail.com>
2022-12-17 13:07:37 +0330

    feat : Add form page header

--- src/components/5-Pages/Form.jsx
+++ src/components/5-Pages/Form.jsx
@@ -1,13 +1,14 @@
+import { Box } from '@mui/material';
 import React, { useEffect } from 'react';
+import Header from '../3-Organisms/Header';
 
 export default function Form ()
 {
-   useEffect( () =>
-   {
-      document.title = "Form Page";
-   }, [] );
+   useEffect( () => { document.title = "Form Page"; }, [] );
 
    return (
-      <div>Form</div>
+      <Box m="20px">
+         <Header title="CREATE USER" subtitle="Create a New User Profile" />
+      </Box>
    );
 }
