95f7e490aa14e7a71b4a81ed0d828cb16de9c93c
yaroofie <yaroofie@gmail.com>
2022-12-17 15:01:20 +0330

    feat : Add faq page header

--- src/components/5-Pages/FAQ.jsx
+++ src/components/5-Pages/FAQ.jsx
@@ -1,10 +1,14 @@
+import { Box } from '@mui/material';
 import React, { useEffect } from 'react';
+import Header from "../3-Organisms/Header";
 
 export default function FAQ ()
 {
-   useEffect( () => { document.title = "Form Page"; }, [] );
+   useEffect( () => { document.title = "FAQ Page"; }, [] );
 
    return (
-      <div>FAQ</div>
+      <Box m="20px">
+         <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+      </Box>
    );
 }
