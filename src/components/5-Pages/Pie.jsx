db5a9ce02474ed12ed84920a28cad5dbe321481e
yaroofie <yaroofie@gmail.com>
2022-12-17 15:18:35 +0330

    feat : Add pie page header

--- src/components/5-Pages/Pie.jsx
+++ src/components/5-Pages/Pie.jsx
@@ -1,13 +1,14 @@
+import { Box } from "@mui/material";
 import React, { useEffect } from 'react';
+import Header from "../3-Organisms/Header";
 
 export default function Pie ()
 {
-   useEffect( () =>
-   {
-      document.title = "Pie Page";
-   }, [] );
+   useEffect( () => { document.title = "Pie Page"; }, [] );
 
    return (
-      <div>Pie</div>
+      <Box m="20px">
+         <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+      </Box>
    );
 }
