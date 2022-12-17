024589e645093e1634472cc763ff172b0f3feba5
yaroofie <yaroofie@gmail.com>
2022-12-17 09:23:23 +0330

    feat : Add theme settings for dark and light mode

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,23 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "../scripts/theme";
+
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./5-Pages/Dashboard";
-export default function App(){
+export default function App ()
+{
+  const [ theme, colorMode ] = useMode();
   return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </div>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
