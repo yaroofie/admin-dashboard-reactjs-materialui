90eaf40af06666393b801930a7128dd980cf455c
yaroofie <yaroofie@gmail.com>
2022-12-17 15:29:02 +0330

    feat : Add line chart component

--- src/components/5-Pages/Line.jsx
+++ src/components/5-Pages/Line.jsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import React, { useEffect } from 'react';
 import Header from "../3-Organisms/Header";
+import LineChart from "../3-Organisms/LineChart";
 
 export default function Line ()
 {
@@ -12,6 +13,9 @@ export default function Line ()
    return (
       <Box m="20px">
          <Header title="Line Chart" subtitle="Simple Line Chart" />
+         <Box height="75vh">
+            <LineChart />
+         </Box>
       </Box>
    );
 }
