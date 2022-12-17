7919832e5da0ef8f2bc8876408e73ed928354670
yaroofie <yaroofie@gmail.com>
2022-12-17 12:48:19 +0330

    feat : Add contacts page header

--- src/components/5-Pages/Contacts.jsx
+++ src/components/5-Pages/Contacts.jsx
@@ -1,10 +1,14 @@
+import { Box } from "@mui/material";
 import React, { useEffect } from 'react';
+import Header from "../3-Organisms/Header";
 
 export default function Contacts ()
 {
    useEffect( () => { document.title = "Contacts Page"; }, [] );
 
    return (
-      <div>Contacts</div>
+      <Box m="20px">
+         <Header title="CONTACTS" subtitle="List of Contacts for Future Reference" />
+      </Box>
    );
 }
