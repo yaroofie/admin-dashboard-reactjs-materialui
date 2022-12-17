e0ec3ee6226f9e076ef00d141325fead2cf0912c
yaroofie <yaroofie@gmail.com>
2022-12-17 12:24:00 +0330

    feat : Add dashboard header

--- src/components/5-Pages/Dashboard.jsx
+++ src/components/5-Pages/Dashboard.jsx
@@ -1,10 +1,16 @@
+import { Box } from '@mui/material';
 import React, { useEffect } from 'react';
+import Header from "../3-Organisms/Header"
 
 export default function Dashboard ()
 {
    useEffect( () => { document.title = "Dashboard Page"; }, [] );
 
    return (
-      <div>Dashboard</div>
+      <Box m="20px">
+         <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+         </Box>
+      </Box>
    );
 }
