133cf81f64c385c5775724318e38232f0b1fa6a4
yaroofie <yaroofie@gmail.com>
2022-12-17 12:59:12 +0330

    feat : Add invoices page header

--- src/components/5-Pages/Invoices.jsx
+++ src/components/5-Pages/Invoices.jsx
@@ -1,13 +1,14 @@
+import { Box } from "@mui/material";
 import React, { useEffect } from 'react';
+import Header from "../3-Organisms/Header";
 
 export default function Invoices ()
 {
-   useEffect( () =>
-   {
-      document.title = "Invoices Page";
-   }, [] );
+   useEffect( () => { document.title = "Invoices Page"; }, [] );
 
    return (
-      <div>Invoices</div>
+      <Box m="20px">
+         <Header title="INVOICES" subtitle="List of Invoice Balances" />
+      </Box>
    );
 }
