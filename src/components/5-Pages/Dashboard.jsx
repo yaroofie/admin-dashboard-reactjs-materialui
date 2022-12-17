5dbfce2b3026ea29580f7ca9eaf4ef3fb878d347
yaroofie <yaroofie@gmail.com>
2022-12-17 16:07:23 +0330

    feat : Add income row to dashboard

--- src/components/5-Pages/Dashboard.jsx
+++ src/components/5-Pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, useTheme } from '@mui/material';
+import { Box, Button, IconButton, Typography, useTheme } from '@mui/material';
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 import EmailIcon from "@mui/icons-material/Email";
 import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
@@ -6,7 +6,9 @@ import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import TrafficIcon from "@mui/icons-material/Traffic";
 import React, { useEffect } from 'react';
 import { tokens } from "../../scripts/theme";
+import { mockTransactions } from "../../data/mockData";
 import Header from "../3-Organisms/Header";
+import LineChart from "../3-Organisms/LineChart";
 import StatBox from "../3-Organisms/StatBox";
 
 export default function Dashboard ()
@@ -118,6 +120,98 @@ export default function Dashboard ()
                      />
                   }
                />
+            </Box>
+
+            {/* ROW 2 */}
+            <Box
+               gridColumn="span 8"
+               gridRow="span 2"
+               backgroundColor={colors.primary[ 400 ]}
+            >
+               <Box
+                  mt="25px"
+                  p="0 30px"
+                  display="flex "
+                  justifyContent="space-between"
+                  alignItems="center"
+               >
+                  <Box>
+                     <Typography
+                        variant="h5"
+                        fontWeight="600"
+                        color={colors.grey[ 100 ]}
+                     >
+                        Revenue Generated
+                     </Typography>
+                     <Typography
+                        variant="h3"
+                        fontWeight="bold"
+                        color={colors.greenAccent[ 500 ]}
+                     >
+                        $59,342.32
+                     </Typography>
+                  </Box>
+                  <Box>
+                     <IconButton>
+                        <DownloadOutlinedIcon
+                           sx={{ fontSize: "26px", color: colors.greenAccent[ 500 ] }}
+                        />
+                     </IconButton>
+                  </Box>
+               </Box>
+               <Box height="250px" m="-20px 0 0 0">
+                  <LineChart isDashboard={true} />
+               </Box>
+            </Box>
+            <Box
+               gridColumn="span 4"
+               gridRow="span 2"
+               backgroundColor={colors.primary[ 400 ]}
+               overflow="auto"
+            >
+               <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  borderBottom={`4px solid ${ colors.primary[ 500 ] }`}
+                  colors={colors.grey[ 100 ]}
+                  p="15px"
+               >
+                  <Typography color={colors.grey[ 100 ]} variant="h5" fontWeight="600">
+                     Recent Transactions
+                  </Typography>
+               </Box>
+               {mockTransactions.map( ( transaction, i ) => (
+                  <Box
+                     key={`${ transaction.txId }-${ i }`}
+                     display="flex"
+                     justifyContent="space-between"
+                     alignItems="center"
+                     borderBottom={`4px solid ${ colors.primary[ 500 ] }`}
+                     p="15px"
+                  >
+                     <Box>
+                        <Typography
+                           color={colors.greenAccent[ 500 ]}
+                           variant="h5"
+                           fontWeight="600"
+                        >
+                           {transaction.txId}
+                        </Typography>
+                        <Typography color={colors.grey[ 100 ]}>
+                           {transaction.user}
+                        </Typography>
+                     </Box>
+                     <Box color={colors.grey[ 100 ]}>{transaction.date}</Box>
+                     <Box
+                        backgroundColor={colors.greenAccent[ 500 ]}
+                        p="5px 10px"
+                        borderRadius="4px"
+                     >
+                        ${transaction.cost}
+                     </Box>
+                  </Box>
+               ) )}
             </Box>
          </Box>
       </Box>
