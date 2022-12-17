8427a9868ee20ac11f6226b0b7818721314a9cc7
yaroofie <yaroofie@gmail.com>
2022-12-17 12:09:15 +0330

    feat : Add sidebar pro for panel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "../scripts/theme";
 import { Route, Routes } from "react-router-dom";
+import { ProSidebarProvider } from "react-pro-sidebar";
 
 import PanelTemplate from './4-Templates/PanelTemplate';
 
@@ -23,21 +24,23 @@ export default function App ()
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <PanelTemplate>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/invoices" element={<Invoices />} />
-            <Route path="/form" element={<Form />} />
-            <Route path="/bar" element={<Bar />} />
-            <Route path="/pie" element={<Pie />} />
-            <Route path="/line" element={<Line />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/geography" element={<Geography />} />
-          </Routes>
-        </PanelTemplate>
+        <ProSidebarProvider>
+          <PanelTemplate>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </PanelTemplate>
+        </ProSidebarProvider>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
