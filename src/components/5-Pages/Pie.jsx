32db74a7200123b04a064269cbc8bd25070c4769
yaroofie <yaroofie@gmail.com>
2022-12-17 15:22:46 +0330

    feat : Add pie chart component

--- src/components/5-Pages/Pie.jsx
+++ src/components/5-Pages/Pie.jsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import React, { useEffect } from 'react';
 import Header from "../3-Organisms/Header";
+import PieChart from "../3-Organisms/PieChart";
 
 export default function Pie ()
 {
@@ -9,6 +10,9 @@ export default function Pie ()
    return (
       <Box m="20px">
          <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+         <Box height="75vh">
+            <PieChart />
+         </Box>
       </Box>
    );
 }
