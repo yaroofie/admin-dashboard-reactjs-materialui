4d8b763b601da2d04178f616f2c70aaac595f53d
yaroofie <yaroofie@gmail.com>
2022-12-17 15:13:44 +0330

    feat : Add bar page header

--- src/components/5-Pages/Bar.jsx
+++ src/components/5-Pages/Bar.jsx
@@ -1,10 +1,14 @@
+import { Box } from "@mui/material";
 import React, { useEffect } from 'react';
+import Header from "../3-Organisms/Header";
 
 export default function Bar ()
 {
    useEffect( () => { document.title = "Bar Page"; }, [] );
 
    return (
-      <div>Bar</div>
+      <Box m="20px">
+         <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+      </Box>
    );
 }
